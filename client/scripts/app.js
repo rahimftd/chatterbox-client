ca5127bd827c1c1fefbcf2de7f61ea3404275701
rahimftd <rahim.dharssi@gmail.com>
2016-03-14 16:34:02 -0700

    sending new messages to server successfully

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,7 +4,7 @@ var app = {
 
 app.init = function() {
   $('#main').find('.username').on('click', this.addFriend);
-  $('#send .submit').on('submit', this.handleSubmit);
+  $('.message-area').on('submit', this.handleSubmit);
 };
 app.send = function(message) {
   $.ajax({
@@ -15,6 +15,7 @@ app.send = function(message) {
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
+      $('#message-input').val('Enter message here...');
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -29,7 +30,7 @@ app.fetch = function() {
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      console.log(data);
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -45,12 +46,42 @@ app.addMessage = function(message) {
   $('#chats').append(node);
 };
 app.addRoom = function(roomName) {
-  var node = '<li class="' + roomName + ' room-name">' + roomName + '</li>';
+  var node = '<option value="' + roomName + '">' + roomName + '</option>';
   $('#roomSelect').append(node);
 };
 app.addFriend = function(friend) {
   return true;
 };
-app.handleSubmit = function() {
-  return true;
+// Runs whenever the click handler is clicked
+app.handleSubmit = function(event) {
+  event.preventDefault();
+  var text = $('#message-input').val();
+  var room = $('#roomSelect').val();
+  var username = app.getUrlParameter('username');
+  var message = app.createMessageObject(text, room, username);
+  app.send(message);
 };
+app.createMessageObject = function(text, room, username) {
+  var obj = {
+    username: username,
+    text: text,
+    roomname: room
+  };
+  return obj;
+};
+
+app.getUrlParameter = function (sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1));
+  var sURLVariables = sPageURL.split('&');
+  var sParameterName;
+
+  for (var i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
+
+app.init();
